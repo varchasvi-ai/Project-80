2b922693ae43c7d6a2440df296876475b509c36b
varchasvi-ai <76240712+varchasvi-ai@users.noreply.github.com>
2021-04-21 20:43:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function getParagraph1() {
+    var inputs = [];
+    for(var i = 1; i <=6; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_" + i).value);
+        document.getElementById("showParagraph1").innerHTML = inputs.join(". ");
+    
+    }
+    console.log(inputs);
+}
+
+function getParagraph2() {
+    var inputs = [];
+    for(var i = 1; i <=6; i++)
+    {
+        inputs.push(document.getElementById("para2_input_box_" + i).value);
+        document.getElementById("showParagraph2").innerHTML = inputs.join(". ");
+    
+    }
+    console.log(inputs);
+}
+
